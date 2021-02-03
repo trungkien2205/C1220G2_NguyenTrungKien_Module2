10f0a342aabb4b28ccef7c882b1ec40e253479dc
John Smith <example@email.com>
2021-02-03 14:37:50 +0700

    thuc hanh

---
+++ src/_03_array_method/thuc_hanh/FindMin.java
@@ -0,0 +1,19 @@
+package _03_array_method.thuc_hanh;
+
+public class FindMin {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + index);
+    }
+
+    public static int minValue(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min=array[i];
+            }
+        }
+        return min;
+    }
+}
