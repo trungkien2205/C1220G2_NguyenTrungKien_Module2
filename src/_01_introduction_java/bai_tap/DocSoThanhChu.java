efae4eb6ff21198b4649804f1767da7481a7d490
John Smith <example@email.com>
2021-02-03 08:15:57 +0700

    update

---
+++ src/_01_introduction_java/bai_tap/DocSoThanhChu.java
@@ -0,0 +1,10 @@
+package _01_introduction_java.bai_tap;
+
+import java.util.Scanner;
+
+public class DocSoThanhChu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int str= scanner.nextInt();
+    }
+}
